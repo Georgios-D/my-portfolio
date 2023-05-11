33ce159659cecd1822200b09edfd38fb9eb80d3d
Georgios.D <georgios_97@hotmail.com>
2023-05-11 19:54:55 +0200

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
                         <img className="h-20 w-20 " src="GD.png"></img>
                     </a>
                 </a>
-                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
                     <a href="#projects" className="mr-5 hover:text-white">
                         Past Work
                     </a>
